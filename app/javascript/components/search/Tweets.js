e1d6cc29bb43659c6560122138ef807903babf7c
Awtanabe <panser098@gmail.com>
2020-07-13 22:50:38 +0900

    タブの切り替え

--- app/javascript/components/search/Tweets.js
+++ app/javascript/components/search/Tweets.js
@@ -1,6 +1,46 @@
 import React from "react"
 import Tweet from '../tweets/tweet'
 
+
+const TAB_TYPES = {
+  FOR_YOU: 'for_you',
+  COVID_19: 'covid_19',
+  TRANDING: 'tranding',
+  NEWS: 'news'
+}
+
+const tabData = [
+  {
+    text: 'for_you',
+    type: TAB_TYPES.FOR_YOU
+  },
+  {
+    text: 'covid_19',
+    type: TAB_TYPES.COVID_19
+  },
+  {
+    text: 'tranding',
+    type: TAB_TYPES.TRANDING
+  },
+  {
+    text: 'News',
+    type: TAB_TYPES.NEWS
+  }
+];
+
+const Tabs = ({cuurentTabType, tabData, onClick}) => (
+  <ul>
+    {tabData.map(tab => (
+      <li 
+        className={cuurentTabType === tab.type ? 'active' : ''}
+        onClick={() => onClick(tab.type)}
+      >
+       {tab.text}
+      </li>
+      ))}
+  </ul>
+)
+
 class Tweets extends React.Component {
   constructor(props) {
     super(props)
@@ -8,7 +48,8 @@ class Tweets extends React.Component {
       initialTweets: [],
       filterdTweets: [],
       value: '',
-      image: ''
+      image: '',
+      cuurentTabType: TAB_TYPES.FOR_YOU
     }
   }
 
@@ -24,6 +65,32 @@ class Tweets extends React.Component {
     const tweets = this.state.initialTweets
     const filterd_tweets = tweets.filter((tweet) => tweet.body.includes(keyword))      
     this.setState({filterdTweets: filterd_tweets})  
+  }
+
+  changeTab(tabType) {
+    this.setState({cuurentTabType: tabType})
+  }
+
+  renderContents() {    
+     let body = null
+     if (this.state.cuurentTabType =='for_you') {
+       body = (<div className="for-you">
+          <h3><img src="/assets/search-image-sample.png" width="407" height="200"/></h3>
+         </div>
+         )
+     } else if (this.state.cuurentTabType =='covid_19') {
+       body = (<div className="covid-19">covid_19
+       </div>)
+     } else if (this.state.cuurentTabType == 'tranding') {
+       body = (<div className="tranding">tranding
+        </div>
+       )
+     } else if (this.state.cuurentTabType == 'news') {
+       body = (    <div className="news">news</div>)
+     }
+     
+
+    return (body)
   }
 
   render () {
@@ -60,24 +127,12 @@ class Tweets extends React.Component {
 
           </div>
           <div className="main-topicts">
-            <ul>
-              <li>for you</li>
-              <li>covid-19</li>
-              <li>tranding</li>
-              <li>news</li>
-            </ul>
-            <div className="for-you">
-            <h3><img src="/assets/search-image-sample.png" width="407" height="200"/></h3>
-
-            </div>
-            <div className="covid-19">
-
-            </div>
-            <div className="tranding">
-
-            </div>
-            <div className="news">
-            </div>
+            <Tabs
+              cuurentTabType={this.state.cuurentTabType}
+              tabData={tabData}
+              onClick={(e) => this.changeTab(e)}
+            />
+            {this.renderContents()}
           </div>
           <div className="tweets">
             {filterdTweets.map((tweet) => <Tweet key={tweet.id} tweet={tweet}/>)}
